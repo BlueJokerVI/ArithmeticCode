b98296cb6dd7733886c0c8670d86d794e76f0f0c
BlueCat <15757048847@163.com>
2024-03-15 18:34:17 +0800

    搜索二叉树，删除节点

--- src/main/java/_007_Tree/DeleteNode.java
+++ src/main/java/_007_Tree/DeleteNode.java
@@ -13,12 +13,14 @@ public class DeleteNode {
         }
 
         TreeNode node = new TreeNode(1);
+        node.left = root;
         dfs(root,node,true,key);
 
         return node.left;
     }
 
 
+    //找左子树最小值替换被删除的位置0.o
     void dfs(TreeNode node, TreeNode father, boolean LR, int key) {
         if (node == null) {
             return;
@@ -26,9 +28,9 @@ public class DeleteNode {
 
         if (node.val > key) {
             dfs(node.left, node, true, key);
-        } else if (node.val < key)
+        } else if (node.val < key) {
             dfs(node.right, node, false, key);
-        else {
+        } else {
             //无右子树
             if(node.right==null){
                 if(LR){
@@ -36,12 +38,10 @@ public class DeleteNode {
                 }else {
                     father.right = node.left;
                 }
-            }else if(node.right.left==null&&node.right.right==null){
-                //右子树就一个节点
-
+            }else if(node.right.left==null){
+                //右子树,没有左子树
                 TreeNode cNode = node.right;
                 cNode.left = node.left;
-                cNode.right = null;
 
                 if(LR){
                     father.left = cNode;
@@ -73,7 +73,47 @@ public class DeleteNode {
         }
 
         TreeNode v = tmp.left;
-        tmp.left = null;
+        tmp.left = v.right;
         return v;
+    }
+
+
+    /**
+     * 第一种情况：没找到删除的节点，遍历到空节点直接返回了
+     * 找到删除的节点
+     * 第二种情况：左右孩子都为空（叶子节点），直接删除节点， 返回NULL为根节点
+     * 第三种情况：删除节点的左孩子为空，右孩子不为空，删除节点，右孩子补位，返回右孩子为根节点
+     * 第四种情况：删除节点的右孩子为空，左孩子不为空，删除节点，左孩子补位，返回左孩子为根节点
+     * 第五种情况：左右孩子节点都不为空，则将删除节点的左子树头结点（左孩子）放到删除节点的右子树的最左面节点的左孩子上，返回删除节点右孩子为新的根节点。
+     * @param root
+     * @param key
+     * @return
+     */
+    public TreeNode deleteNode1(TreeNode root, int key) {
+        if (root == null) {
+            return root;
+        }
+        if (root.val == key) {
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            } else {
+                TreeNode cur = root.right;
+                while (cur.left != null) {
+                    cur = cur.left;
+                }
+                cur.left = root.left;
+                root = root.right;
+                return root;
+            }
+        }
+        if (root.val > key) {
+            root.left = deleteNode1(root.left, key);
+        }
+        if (root.val < key) {
+            root.right = deleteNode1(root.right, key);
+        }
+        return root;
     }
 }
