24f7abe20efc083fa66aff21cf54e285d030d7bc
BlueCat <15757048847@163.com>
2024-05-23 17:42:09 +0800

    Hash

--- src/main/java/_003_Hash/ContainsNearbyDuplicate.java
+++ src/main/java/_003_Hash/ContainsNearbyDuplicate.java
@@ -1,7 +1,9 @@
 package _003_Hash;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @BelongsProject: ArithmeticCode
@@ -10,12 +12,20 @@ import java.util.Map;
  */
 public class ContainsNearbyDuplicate {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        Map<Integer,Integer> cnt = new HashMap<>();
+        Set<Integer> has = new HashSet<>();
         int l=0,r=0;
         while (r<nums.length){
-
+            if(r-l<=k){
+                if(has.contains(nums[r])){
+                    return true;
+                }
+                has.add(nums[r]);
+                r++;
+            }else {
+                has.remove(nums[l]);
+                l++;
+            }
         }
-
         return false;
     }
 }
