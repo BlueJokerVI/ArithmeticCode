36897b8c05c6831477654e493250670b78a4003d
redsky <1545860023@qq.com>
2024-05-31 13:29:16 +0800

    先序、后续、中序、二叉树构建

--- src/main/java/_007_Tree/Test.java
+++ src/main/java/_007_Tree/Test.java
@@ -70,5 +70,49 @@ public class Test {
         return p.val == q.val && check(p.left, q.right) && check(p.right, q.left);
     }
 
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return buildTree(preorder,0,preorder.length-1,inorder,0,inorder.length-1);
+    }
+
+    private TreeNode buildTree(int[] preorder, int pl,int pr,int[] inorder,int il,int ir){
+        if(pl > pr){
+            return null;
+        }
+        TreeNode cur = new TreeNode();
+        cur.val = preorder[pl];
+        int index = il;
+        while (cur.val!=inorder[index]){
+            index++;
+        }
+        //左子树元素个数
+        int lenL = index - il;
+        int lenR = ir - index;
+        cur.left = buildTree(preorder, pl+1, pl+lenL, inorder, il, index-1);
+        cur.right = buildTree(preorder, pl+lenL+1, pr, inorder, index+1, ir);
+        return cur;
+    }
+
+
+    public TreeNode buildTree1(int[] inorder, int[] postorder) {
+        return buildTreeWithI_P(inorder,0,inorder.length-1,postorder,0,postorder.length-1);
+    }
+
+    private TreeNode buildTreeWithI_P(int[] inorder,int il,int ir ,int[] postorder,int pl,int pr){
+        if(il > ir){
+            return null;
+        }
+        TreeNode cur = new TreeNode();
+        cur.val = postorder[pr];
+        int index = il;
+        while (cur.val!=inorder[index]){
+            index++;
+        }
+        //左子树元素个数
+        int lenL = index - il;
+
+        cur.left = buildTreeWithI_P(inorder,il,index-1,postorder,pl,pl+lenL-1);
+        cur.right = buildTreeWithI_P(inorder,index+1,ir,postorder,pl+lenL,pr-1);
+        return cur;
+    }
 
 }
